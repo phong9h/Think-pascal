bb5409426d52c80f515d9996690893049745e5e4
phong <tranthephong33@gmail.com>
2016-01-10 07:50:33 +0700

    First commit

---
+++ bad_printTentimes.pas
@@ -0,0 +1,18 @@
+uses crt;
+var message: string;
+begin
+  clrscr;
+  write('Input your message: ');
+  readln(message);
+  writeln(message);
+  writeln(message);
+  writeln(message);
+  writeln(message);
+  writeln(message);
+  writeln(message);
+  writeln(message);
+  writeln(message);
+  writeln(message);
+  writeln(message);
+  readln
+end.
